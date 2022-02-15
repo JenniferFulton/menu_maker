7df91fb57cfbd2effd2b221921eff38a0e16e604
JenniferFulton <jennifer.reeves8@gmail.com>
2022-02-15 14:02:18 -0500

    login and planner linked with routes and successfully logged in and rendered to home page of planner app

---
+++ menu/login/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+import re
+import bcrypt
+import datetime
+
+class UserManager(models.Manager):
+    def register_validator(self, postData):
+    #Validate user entry upon registration 
+        errors = {}
+        check_user = User.objects.filter(email = postData['email'])
+
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = 'First name should be atleast 2 characters'
+
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = 'Last name should be atleast 2 characters'
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = 'Invalid email address'
+        
+        if len(check_user) != 0:
+            errors['duplicate_email'] = 'Email already registered, please use a different email to resister'
+        
+
+        if len(postData['password']) < 8:
+            errors['password'] = 'Password must be atleast 8 characters'
+
+        if postData['password'] != postData['confirm_pw']:
+            errors['confirm_pw'] = 'Passwords do not match'
+        
+        return errors
+    
+    def login_validator(self, postData):
+    #Validates a user trying to log in 
+        errors = {}
+        check_user = User.objects.filter(email = postData['logemail'])
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['logemail']):
+            errors['email'] = 'Please provide valid email address'
+        
+        if len(check_user) == 0:
+            errors['no_user'] = 'User does not exist, please register first'
+
+        elif not bcrypt.checkpw(postData['logpassword'].encode(), check_user[0].password.encode()):
+            errors['invalid_pw'] = 'Password does not match username'
+
+        return errors
+    
+    def update_validator(self, postData):
+        #Validates user entry when they update their profile 
+        errors = {}
+        check_user = User.objects.filter(email = postData['new_email'])
+
+        if len(postData['new_first_name']) < 2:
+            errors['first_name'] = 'First name should be atleast 2 characters'
+
+        if len(postData['new_last_name']) < 2:
+            errors['last_name'] = 'Last name should be atleast 2 characters'
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['new_email']):
+            errors['email'] = 'Invalid email address'
+        
+        if len(check_user) != 0:
+            errors['duplicate_email'] = 'Email already registered, please use a different email to resister'
+        
+        return errors
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True, null=True)
+    objects = UserManager()
---
+++ menu/login/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
+import bcrypt
+
+def registration_login(request):
+    #root route will have a form for registration and form for logging in
+    return render(request, 'reg_login.html')
+
+def register(request):
+    #'/register' will create a new user and redirect back to login page
+    if request.method == "POST":
+        errors = User.objects.register_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        
+        else:
+            hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+            User.objects.create(
+                first_name = request.POST['first_name'],
+                last_name = request.POST['last_name'],
+                email = request.POST['email'],
+                password = hashed_pw,
+            )
+            new_user = User.objects.last()
+            request.session['user'] = new_user.id
+            return redirect('/success')
+        
+
+def login(request):
+    #'/login' will verify email and password to allow user to login
+    #if no errors return redirect('/success')
+    #if errors return redirect('/')
+    if request.method == "POST":
+        errors = User.objects.login_validator(request.POST)
+        if len(errors) > 0:
+            for key,value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+
+        else:
+            logged_user = User.objects.get(email=request.POST['logemail'])
+            request.session['user'] = logged_user.id
+            return redirect('/success')
+
+def success(request):
+    #'/success' will redirect to examone app where the home page of the user will be with quotes
+    if 'user' not in request.session:
+        return redirect('/')
+    else:
+        return redirect('planner/')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
