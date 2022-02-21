d5c054890ee109a732aa58bc4844888bcae6e29c
JenniferFulton <jennifer.reeves8@gmail.com>
2022-02-21 11:05:40 -0500

    login functioning, made neater. Planner home page updated

--- menu/planner/views.py
+++ menu/planner/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render, redirect
+from login.models import *
 
 def home_page(request):
+    #Checks if user is logged in
+    if 'user' not in request.session:
+        return redirect('/')
+    
+    #will be redirected to home page once logged in or registered
+    active_user = User.objects.get(id = request.session['user'])
+    context = {
+        
+    }
     return render(request,'home_page.html')
