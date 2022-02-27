b3a01cdebf93fcc4c21e0467a554a65f0d5d1495
JenniferFulton <jennifer.reeves8@gmail.com>
2022-02-27 18:00:56 -0500

    Recipe model created, no migrations have been made yet

--- menu/planner/models.py
+++ menu/planner/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=255)
+    ingredients = models.CharField(max_length=255)
+    instructions = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    # objects = RecipeManager()
+    
