c596022cc82377579118bb4d1f40945fa70ec846
JenniferFulton <jennifer.reeves8@gmail.com>
2022-03-01 17:31:03 -0500

    recipe model ipdated and menu model created

--- menu/planner/models.py
+++ menu/planner/models.py
@@ -2,9 +2,24 @@ from django.db import models
 
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=255)
-    ingredients = models.CharField(max_length=255)
-    instructions = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    prep_time = models.DurationField()
+    cook_time = models.DurationField()
+    ingredients = models.TextField()
+    directions = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     # objects = RecipeManager()
-    
+
+class Menu(models.Model):
+    week_date = models.DateField()
+    mon = models.ForeignKey()
+    tues = models.ForeignKey()
+    wed = models.ForeignKey()
+    thrus = models.ForeignKey()
+    fri = models.ForeignKey()
+    sat = models.ForeignKey()
+    sun = models.ForeignKey()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    # objects = MenuManager()
